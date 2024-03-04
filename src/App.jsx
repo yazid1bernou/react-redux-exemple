789b42d2da2fa7be3395fd254bf4e4958bc72cf4
unknown <yazid1bernou@gmail.com>
2024-03-04 09:59:31 +0000

    termineted with react redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import { allFlags , singleFlag } from './redux/actions/FlagsAction'
 
 function App() {
     
   const dispatch = useDispatch();
+  const allDataflags =  useSelector(state => state.flags);
+  const singleDataflag = useSelector(state => state.flag);
 
   const handleLogin = () => {
     dispatch({
@@ -21,11 +24,13 @@ function App() {
   };
 
   //---------------Display flags and flag --------------------------------------------------
-  const handleFlags = () => {
-     
+  const handleFlags = async() => {
+     await dispatch(allFlags())
+     console.log(allDataflags.flags)
   }
-  const handleFlag = () => {
-    
+  const handleFlag = async() => {
+     await dispatch(singleFlag())
+     console.log(singleDataflag.flag)
   }
   return (
     <>
@@ -37,6 +42,30 @@ function App() {
        <h2> Display Flags and flag </h2>
        <button onClick={handleFlags}> Flags</button>
        <button onClick={handleFlag}>Flag</button>
+       <h2> Dispaly all flags</h2>
+       <hr></hr>
+          <div>
+             {allDataflags.flags.map( ({title , thumbnailUrl}) => (
+                  <>
+                     <p> {title}</p>
+                     <img src={thumbnailUrl} width="20px"/>
+               
+                  </>
+                ))
+              } 
+        
+         </div>
+         <h2> Display single flag</h2>
+         <hr></hr>
+         <div> 
+           {/*  {singleDataflag.flag.map(({title , thumbnailUrl}) => (
+               <>
+                <p> {title}</p>
+                     <img src={thumbnailUrl} width="100px"/>
+               </>
+            ))
+           } */}
+         </div>
     </>
   )
 }
--- src/redux/actions/FlagsAction.jsx
+++ src/redux/actions/FlagsAction.jsx
@@ -1,9 +1,22 @@
 import axios from "axios"
 
-export const  allFlags = () => {
-       
+export const  allFlags = () => dispatch=> {
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+          .then(result => {
+            dispatch({
+                type : "FETCH_ALL_FLAGS",
+                payload : result.data
+            })
+          })
 }
 
-export const singleFlag =  () => {
+export const singleFlag =  () => dispatch => {
+     axios.get('https://jsonplaceholder.typicode.com/photos/1')
+     .then(result =>{
+         dispatch({
+            type : "FETCH_SINGLE_FLAG",
+            payload : result.data
+         })
+     })
 
 }
